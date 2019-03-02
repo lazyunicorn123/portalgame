3f37f0a793414c51c7241629e11bea6567d31fcb
lazyunicorn123 <sweetberries2008@gmail.com>
2019-03-02 11:21:35 -0800

    initial
    hh

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(0.0f, 0.0f, transform.position.z + Speed * Time.deltaTime);
+        }
+	}
+}
