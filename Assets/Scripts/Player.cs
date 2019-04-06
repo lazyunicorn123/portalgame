36988dc66651553e4abe25a0de33ab978143e891
lazyunicorn123 <sweetberries2008@gmail.com>
2019-04-06 11:24:01 -0700

    added crosshair and water

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,18 @@ public class Player : MonoBehaviour
 {
     public Camera myCamera;
     public float Speed;
-    public float Sencitivity;
+    public float JumpStrength;
+    public float Sensitivity;
 
     private Vector2 myMouse;
+    private Rigidbody myRigidbody;
+    private bool isGrounded;
 
 	// Use this for initialization
 	void Start ()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        myRigidbody = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -22,23 +26,41 @@ public class Player : MonoBehaviour
         myMouse.x = Input.GetAxis("Mouse X");
         myMouse.y = Input.GetAxis("Mouse Y");
 
-        transform.eulerAngles += new Vector3(0.0f, myMouse.x * Sencitivity * Time.deltaTime, 0.0f);
+        transform.eulerAngles += new Vector3(0.0f, myMouse.x * Sensitivity * Time.deltaTime, 0.0f);
+        Vector3 targetRotation = new Vector3(-myMouse.y * Sensitivity * Time.deltaTime, 0.0f, 0.0f);
+        if(targetRotation.x + myCamera.transform.eulerAngles.x < 90 || targetRotation.x + myCamera.transform.eulerAngles.x > 270)
+        {
+            myCamera.transform.eulerAngles += targetRotation;
+        }
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0.0f, 0.0f,   Speed * Time.deltaTime);
+            transform.Translate(0.0f, 0.0f, Speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0.0f, 0.0f,   -Speed * Time.deltaTime);
+            transform.Translate(0.0f, 0.0f, -Speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-Speed * Time.deltaTime, 0.0f, 0.0f   );
+            transform.Translate(-Speed * Time.deltaTime, 0.0f, 0.0f);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            myRigidbody.AddForce(0.0f, JumpStrength, 0.0f, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
         }
     }
 }
